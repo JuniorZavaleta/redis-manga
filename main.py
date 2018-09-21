47f75dc76063949f60ccd39154e34bfcaf070116
Junior Claudio <juniorclaudiozavaleta@gmail.com>
2018-09-21 16:13:41 -0500

    avance python

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import redis
+import csv
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
+r.flushall()
 
-# Mangas
-r.hmset('M1', {'nombre': "Aisazu Ni Wa Irarenai", 'carpeta': "AisazuNihaIrarenai"})
+with open('./labels.csv') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    reader.next()
+    i = 1
+    for row in reader:
+        r.hmset('L'+str(i), {'esp': row[0], 'eng': row[1]})
+        i = i + 1
 
-# Etiquetas
-r.hmset('L1', {'esp': 'Futbol', 'eng': 'Soccer'})
+with open('./mangas.csv') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    reader.next()
+    i = 1
+    for row in reader:
+        r.hmset('M' + str(i), {'nombre': row[0], 'carpeta': row[1]})
+        i = i + 1
 
-# Paginas de cada manga con sus etiquetas
-r.sadd('M1P1', '1', '2')
+with open('./mangalabels.csv') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    reader.next()
+    i = 1
+    for row in reader:
+        r.sadd('M{0}P{1}'.format(row[0], row[1]), *set(row[2].split('-')))
